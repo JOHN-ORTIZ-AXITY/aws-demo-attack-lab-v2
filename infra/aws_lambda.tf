c0925b5fbe7c60395c8ef35d9ed1356289bb7dad
JOHN-ORTIZ-AXITY <john.ortiz@axity.com>
2024-08-26 15:49:05 -0500

    Update aws_lambda.tf

--- infra/aws_lambda.tf
+++ infra/aws_lambda.tf
@@ -1,45 +1,76 @@
+# IAM Role for Lambda
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "${var.deployment_name}-${random_string.unique_id.result}"
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
-      "Effect": "Allow",
-      "Sid": ""
-      "Action": [
-        "lambda:CreateFunction",
-        "lambda:UpdateFunctionCode",
-        "lambda:UpdateFunctionConfiguration",
-        "iam:PassRole"
-      ],
-       "Resource": "*"
+      "Action": "sts:AssumeRole"
     }
   ]
 }
 EOF
 }
 
+# IAM Policy to Attach to the Role
+resource "aws_iam_policy" "lambda_execution_policy" {
+  name = "${var.deployment_name}_lambda_policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "lambda:CreateFunction",
+        "lambda:UpdateFunctionCode",
+        "lambda:UpdateFunctionConfiguration",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "s3:GetObject"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach policy to the role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_execution_policy.arn
+}
+
+# Lambda Function
 resource "aws_lambda_function" "analysis_lambda" {
-  # lambda have plain text secrets in environment variables
-  filename      = "${path.root}/resources/lambda_function_payload.zip"
-  function_name = "${var.deployment_name}-${random_string.unique_id.result}"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_function_payload.lambda_handler"
-  layers        = [ "arn:aws:lambda:us-east-1:108863513136:layer:requests-layer:7" ]
+  filename         = "${path.root}/resources/lambda_function_payload.zip"
+  function_name    = "${var.deployment_name}-${random_string.unique_id.result}"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_function_payload.lambda_handler"
+  layers           = [ "arn:aws:lambda:us-east-1:108863513136:layer:requests-layer:7" ]
+  source_code_hash = filebase64sha256("${path.root}/resources/lambda_function_payload.zip")
+  runtime          = "python3.9"
 
-  source_code_hash = filebase64sha256("${path.root}/resources/lambda_function_payload.py")
-
-  runtime = "python3.9"
-
+  # Environment variables (Avoid plain-text secrets)
   environment {
     variables = {
-      access_key = "AKIAIOSFODNN7EXAMPLE1"
-      secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+      access_key = var.access_key
+      secret_key = var.secret_key
     }
   }
+}
+
+# Random string resource to generate unique id for resources
+resource "random_string" "unique_id" {
+  length  = 8
+  special = false
 }
