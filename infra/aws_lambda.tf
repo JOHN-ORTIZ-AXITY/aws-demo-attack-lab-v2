183f3687450004bab74a31812f1f46a12598ac16
JOHN-ORTIZ-AXITY <john.ortiz@axity.com>
2024-08-26 17:59:21 -0500

    Update aws_lambda.tf

--- infra/aws_lambda.tf
+++ infra/aws_lambda.tf
@@ -1,45 +1,25 @@
-trigger:
-  branches:
-    include:
-      - main
+resource "aws_lambda_function" "checkov_lambda" {
+  filename         = "s3://your-s3-bucket/lambda_function_payload.zip"
+  function_name    = "checkov_lambda"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "lambda_function_payload.lambda_handler"
+  runtime          = "python3.9"
+  memory_size      = 256
+  timeout          = 10
+}
 
-pool:
-  vmImage: 'ubuntu-latest'
-
-variables:
-  AWS_REGION: 'us-east-1'
-  LAMBDA_FUNCTION_NAME: 'checkov_lambda'
-  PACKAGE_NAME: 'lambda_function_payload.zip'
-
-steps:
-  - task: UsePythonVersion@0
-    inputs:
-      versionSpec: '3.x'
-      addToPath: true
-
-  - script: |
-      python3 -m venv venv
-      source venv/bin/activate
-      pip install checkov
-      deactivate
-      cd venv/lib/python3.9/site-packages
-      zip -r9 ${OLDPWD}/${PACKAGE_NAME} .
-      cd $OLDPWD
-      zip -g ${PACKAGE_NAME} lambda_function_payload.py
-    displayName: 'Package Lambda and Dependencies'
-
-  - task: AmazonWebServices.aws-vsts-tools.AWSCli@1
-    inputs:
-      awsCredentials: '<Your AWS Service Connection>'
-      regionName: $(AWS_REGION)
-      command: 's3 cp $(PACKAGE_NAME) s3://your-s3-bucket/$(PACKAGE_NAME)'
-
-  - task: TerraformInstaller@0
-    inputs:
-      terraformVersion: '1.1.9'
-
-  - script: |
-      terraform init
-      terraform plan -out=tfplan
-      terraform apply -auto-approve tfplan
-    displayName: 'Deploy Lambda with Terraform'
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "checkov_lambda_role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
